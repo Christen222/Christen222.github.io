603f49fb159c7ee7dfc165e3f35436083870bc5d
Christen222 <christen.smartcowan@dcmail.ca>
2026-07-28 15:13:56 -0400

    editing

--- Assignment4_2/script.js
+++ Assignment4_2/script.js
@@ -3,11 +3,6 @@ const thumbBar = document.querySelector(".thumb-bar");
 
 const btn = document.querySelector("button");
 const overlay = document.querySelector(".overlay");
-const displayedImage = document.querySelector(".displayed-img");
-const thumbBar = document.querySelector(".thumb-bar");
-
-const btn = document.querySelector("button");
-const overlay = document.querySelector(".overlay");
 
 // 1. Data object
 const images = [
