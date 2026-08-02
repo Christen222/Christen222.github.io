43a325d7143ec30de552d26a39a22d7b6d946af6
Christen222 <christen.smartcowan@dcmail.ca>
2026-08-02 18:13:21 -0400

    fix???????

--- Assignment4_4/main-finished.js
+++ Assignment4_4/main-finished.js
@@ -124,30 +124,29 @@ while (balls.length < 25) {
   );
   balls.push(ball);
 }
-
 const para = document.querySelector("p");
-let count = balls.length;
-para.textContent = "Ball count: " + count;
+    let count = balls.length;
+    para.textContent = "Ball count: " + count;
 
-const evilCircle = new EvilCircle(random(0, width), random(0, height));
+    const evilCircle = new EvilCircle(random(0, width), random(0, height));
 
-function loop() {
-  ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
-  ctx.fillRect(0, 0, width, height);
+    function loop() {
+      ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
+      ctx.fillRect(0, 0, width, height);
 
-  for (const ball of balls) {
-    if (ball.exists) {
-      ball.draw();
-      ball.update();
-      ball.collisionDetect();
+      for (const ball of balls) {
+        if (ball.exists) {
+          ball.draw();
+          ball.update();
+          ball.collisionDetect();
+        }
+      }
+
+      evilCircle.draw();
+      evilCircle.checkBounds();
+      evilCircle.collisionDetect();
+
+      requestAnimationFrame(loop);
     }
-  }
 
-  evilCircle.draw();
-  evilCircle.checkBounds();
-  evilCircle.collisionDetect();
-
-  requestAnimationFrame(loop);
-}
-
-loop();
+    loop();
